2520269444a04f577cd065860a9df5c9f424b6b1
sebahernandez <sebaprogramer@gmail.com>
2024-10-25 16:04:32 -0300

    padding home

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ register();
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: "0px 20px 0px 20px" }}>
       <Slider />
       <FeaturedCars />
       <FeaturedSection />
