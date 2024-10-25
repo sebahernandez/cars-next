f85a166f4e9e6eb0a239cbe31d0859f076922ed6
sebahernandez <sebaprogramer@gmail.com>
2024-10-25 15:51:48 -0300

    seo activate en ficha de vehiculos internos

---
+++ app/cars/[name]/[id]/PageCarsClient.jsx
@@ -0,0 +1,188 @@
+"use client"; // Declaramos que es un componente cliente
+
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Map } from "iconic-react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  FreeMode,
+  Autoplay,
+  Thumbs,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default function PageCarsClient({ car }) {
+  const thumbsSwiperRef = useRef(null);
+  const precio = car.price ? car.price.replace(/,/g, ".") : "";
+
+  if (!car) {
+    return <p>Cargando vehículo...</p>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{car.name} - Detalles del Vehículo</title>
+        <meta property="og:title" content={car.name} />
+        <meta property="og:description" content={car.description} />
+        <meta
+          property="og:image"
+          content={car.imageGallery?.[0]?.imageUrl || ""}
+        />
+        <meta
+          property="og:url"
+          content={`https://www.buycars.cl/cars/${car.id}`}
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      <section className="md:container md:mx-auto mt-[120px] sm:mt-36">
+        <div className="py-2 px-5 md:px-0">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-2">{car.name}</h1>
+          <p className="text-gray-700 flex items-center gap-2 font-bold">
+            <Map size="32" color="#2ccce4" />
+            {car.location}
+          </p>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-3 md:mx-0 md:gap-6 mx-5 my-5">
+          <div className="col-span-2 md:col-span-2 lg:row-span-3">
+            <Swiper
+              navigation
+              modules={[Navigation, Pagination, Autoplay, FreeMode, Thumbs]}
+              className="h-[500px] w-full rounded-lg"
+              loop={true}
+              autoplay={{ delay: 2500, disableOnInteraction: false }}
+              thumbs={{
+                swiper:
+                  thumbsSwiperRef.current && !thumbsSwiperRef.current.destroyed
+                    ? thumbsSwiperRef.current
+                    : null,
+              }}
+              breakpoints={{
+                320: { slidesPerView: 1 },
+                640: { slidesPerView: 1 },
+                1024: { slidesPerView: 1 },
+              }}
+            >
+              {car.imageGallery &&
+                Array.isArray(car.imageGallery) &&
+                car.imageGallery.map((image) => (
+                  <SwiperSlide key={car.id}>
+                    <div className="flex h-full w-full items-center justify-center">
+                      <Image
+                        src={image.imageUrl}
+                        alt={image.name}
+                        className="block h-full w-full object-cover"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+            {/* Thumbnails */}
+            <Swiper
+              onSwiper={(swiper) => (thumbsSwiperRef.current = swiper)}
+              loop={true}
+              spaceBetween={10}
+              slidesPerView={4}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mt-4 h-24 w-full"
+              breakpoints={{
+                320: { slidesPerView: 3 },
+                640: { slidesPerView: 3 },
+                1024: { slidesPerView: 5 },
+              }}
+            >
+              {car.imageGallery &&
+                Array.isArray(car.imageGallery) &&
+                car.imageGallery.map((image) => (
+                  <SwiperSlide key={car.id}>
+                    <button className="h-full w-full">
+                      <Image
+                        src={image.imageUrl}
+                        alt={image.description}
+                        className="h-full w-full object-cover"
+                        width={500}
+                        height={500}
+                      />
+                    </button>
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </div>
+          <div className="col-span-2 my-5 md:col-span-2 lg:col-span-3 lg:row-start-4 ">
+            <h2 className="text-xl sm:text-2xl font-bold">Descripción:</h2>
+            <p className="text-justify py-5">{car.description}</p>
+          </div>
+          <div className="col-span-2 md:col-span-2 lg:col-span-2 lg:row-span-3 ">
+            <div className="py-5 flex flex-col items-center sm:items-end">
+              <p className="text-xl sm:text-5xl lg:text-4xl font-bold my-2">
+                ${precio}
+              </p>
+              <Link
+                href="/"
+                className="bg-black text-white rounded p-2 text-center my-3"
+              >
+                Simula tu crédito
+              </Link>
+            </div>
+            <table className="min-w-full table-auto">
+              <thead>
+                <tr>
+                  <th className="px-2 py-2 text-gray-700 font-bold text-left underline text-2xl">
+                    Características:
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border-b">
+                  <td className="px-2 py-2 text-gray-700 font-bold">
+                    Kilómetros:
+                  </td>
+                  <td className="px-2 py-2 text-gray-700">{car.miles}</td>
+                </tr>
+                <tr className="border-b">
+                  <td className="px-2 py-2 text-gray-700 font-bold">
+                    Tipo de combustible:
+                  </td>
+                  <td className="px-2 py-2 text-gray-700">{car.fuelType}</td>
+                </tr>
+                <tr className="border-b">
+                  <td className="px-2 py-2 text-gray-700 font-bold">
+                    Transmisión:
+                  </td>
+                  <td className="px-2 py-2 text-gray-700">
+                    {car.transmission}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-2 py-2 text-gray-700 font-bold">
+                    Disponibilidad:
+                  </td>
+                  <td className="px-2 py-2 text-gray-700">
+                    {car.available ? "Disponible" : "No disponible"}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+PageCarsClient.propTypes = {
+  car: PropTypes.object.isRequired,
+};
--- app/cars/[name]/[id]/page.jsx
+++ app/cars/[name]/[id]/page.jsx
@@ -1,194 +1,32 @@
-"use client";
+import PageCarsClient from "./PageCarsClient"; // Importamos el componente cliente
+import PropTypes from "prop-types";
 
-import { useEffect, useState } from "react";
+// Este es un componente del servidor
+export default async function PageCars({ params }) {
+  const { id } = params; // Obtenemos el id de params
+  const token = process.env.NEXT_PUBLIC_TOKEN_KEY; // Obtenemos el token de la variable de entorno
+  const url = "https://venpu.cl/api"; // La URL de la API
 
-import Image from "next/image";
-import Link from "next/link";
-import { Map } from "iconic-react";
+  // Lógica para obtener los datos del vehículo desde la API
+  const response = await fetch(`${url}/cars/stock/${Number(id)}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  FreeMode,
-  A11y,
-  Autoplay,
-  Thumbs,
-} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-
-const PageCars = ({ params }) => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const [car, setCar] = useState({})
-  const { id } = params; 
-  const precio = car.price ? car.price.replace(/,/g, ".") : "";
-
-  useEffect(() => {
-    if (id) {
-      const fetchData = async () => {
-        try {
-          const token = `${process.env.NEXT_PUBLIC_TOKEN_KEY}`;
-          const url = "https://venpu.cl/api";
-          // Hacemos la solicitud utilizando el `id`
-          const response = await fetch(`${url}/cars/stock/${Number(id)}`, {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-          });
-
-          const r = await response.json();
-          if (r.status) setCar(r.data);
-          else console.log(r.error);
-        } catch (error) {
-          console.error("Error al obtener los datos de la API:", error);
-        }
-      };
-
-      fetchData();
-    }
-  }, [id]);
+  const data = await response.json();
+  const car = data.status ? data.data : null;
 
   if (!car) {
-    return <p>Cargando vehículo...</p>; 
+    return <p>Cargando vehículo...</p>;
   }
 
-  return (
-    <section className="md:container md:mx-auto mt-[120px] sm:mt-36">
-      <div className="py-2 px-5 md:px-0">
-        <h1 className="text-2xl sm:text-3xl font-bold mb-2">{car.name}</h1>
-        <p className="text-gray-700 flex items-center gap-2 font-bold">
-          <Map size="32" color="#2ccce4" />
-          {car.location}
-        </p>
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-3 md:mx-0 md:gap-6 mx-5 my-5">
-        <div className="col-span-2 md:col-span-2 lg:row-span-3">
-          <Swiper
-            navigation
-            modules={[Navigation, Pagination, Autoplay, FreeMode, Thumbs]}
-            className="h-[500px] w-full rounded-lg"
-            loop={true}
-            autoplay={{ delay: 2500, disableOnInteraction: false }}
-            thumbs={{
-              swiper:
-                thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-            }}
-            breakpoints={{
-              320: { slidesPerView: 1 },
-              640: { slidesPerView: 1 },
-              1024: { slidesPerView: 1 },
-            }}
-          >
-            {car &&
-              car.imageGallery &&
-              Array.isArray(car.imageGallery) &&
-              car.imageGallery.map((image, index) => (
-                <SwiperSlide key={index}>
-                  <div className="flex h-full w-full items-center justify-center">
-                    <Image
-                      src={image.imageUrl}
-                      alt={image.name}
-                      className="block h-full w-full object-cover"
-                      width={500}
-                      height={500}
-                    />
-                  </div>
-                </SwiperSlide>
-              ))}
-          </Swiper>
-          {/* Thumbnails */}
-          <Swiper
-            onSwiper={setThumbsSwiper}
-            loop={true}
-            spaceBetween={10}
-            slidesPerView={4}
-            freeMode={true}
-            watchSlidesProgress={true}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className="mt-4 h-24 w-full"
-            breakpoints={{
-              320: { slidesPerView: 3 },
-              640: { slidesPerView: 3 },
-              1024: { slidesPerView: 5 },
-            }}
-          >
-            {car &&
-              car.imageGallery &&
-              Array.isArray(car.imageGallery) &&
-              car.imageGallery.map((image, index) => (
-                <SwiperSlide key={index}>
-                  <button className="h-full w-full">
-                    <Image
-                      src={image.imageUrl}
-                      alt={image.description}
-                      className="h-full w-full object-cover"
-                      width={500} // Asegúrate de ajustar esto a las dimensiones reales de tus imágenes
-                      height={500}
-                    />
-                  </button>
-                </SwiperSlide>
-              ))}
-          </Swiper>
-        </div>
-        <div className="col-span-2 my-5 md:col-span-2 lg:col-span-3 lg:row-start-4 ">
-          <h2 className="text-xl sm:text-2xl font-bold">Descripción:</h2>
-          <p className="text-justify py-5">{car.description}</p>
-        </div>
-        <div className="col-span-2 md:col-span-2 lg:col-span-2 lg:row-span-3 ">
-          <div className="py-5 flex flex-col items-center sm:items-end">
-            <p className="text-xl sm:text-2xl lg:text-4xl font-bold my-2">
-              ${precio}
-            </p>
-            <Link
-              href="/"
-              className="bg-black text-white rounded p-2 text-center my-3"
-            >
-              Simula tu crédito
-            </Link>
-          </div>
-          <table className="min-w-full table-auto">
-            <tbody>
-              <tr className="border-b">
-                <td className="px-2 py-2 text-gray-700 font-bold">
-                  Kilómetros:
-                </td>
-                <td className="px-2 py-2 text-gray-700">{car.miles}</td>
-              </tr>
-              <tr className="border-b">
-                <td className="px-2 py-2 text-gray-700 font-bold">
-                  Tipo de combustible:
-                </td>
-                <td className="px-2 py-2 text-gray-700">{car.fuelType}</td>
-              </tr>
-              <tr className="border-b">
-                <td className="px-2 py-2 text-gray-700 font-bold">
-                  Transmisión:
-                </td>
-                <td className="px-2 py-2 text-gray-700">{car.transmission}</td>
-              </tr>
-              <tr>
-                <td className="px-2 py-2 text-gray-700 font-bold">
-                  Disponibilidad:
-                </td>
-                <td className="px-2 py-2 text-gray-700">
-                  {car.available ? "Disponible" : "No disponible"}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        {/*    <div className="my-5 md:my-0 md:col-span-2 lg:col-span-2 lg:row-span-3 lg:col-start-4 lg:row-start-1">
-          <ContactForm />
-        </div> */}
-      </div>
-    </section>
-  );
-};
+  // Pasamos los datos al componente cliente
+  return <PageCarsClient car={car} />;
+}
 
-export default PageCars;
+// props validation
+PageCars.propTypes = {
+  params: PropTypes.object.isRequired,
+};
